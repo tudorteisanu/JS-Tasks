dc2e3ffdf58f52e539ff4848b333dd2697e1ae15
Denis Postolachi <deny.postolachi88@gmail.com>
2021-08-23 01:44:44 +0300

    fifth task code styling correct

---
+++ Задача №5 - строки/script/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const extractCurrencyValue = function cutCurrencySignFirstPos(str) {
+  const signCut = str.slice(1);
+  return parseInt(signCut);
+};
+
+alert(extractCurrencyValue("$120"));
