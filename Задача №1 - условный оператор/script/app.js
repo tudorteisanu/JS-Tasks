3629101b280e5046accab9f9be8fba23ee2a609e
Denis Postolachi <deny.postolachi88@gmail.com>
2021-08-23 01:10:50 +0300

    initial commit

---
+++ Задача №1 - условный оператор/script/app.js
@@ -0,0 +1,33 @@
+const n = 9;
+
+switch (n) {
+  case 1:
+    alert("Zdesi 1");
+    break;
+  case 2:
+    alert("Zdesi 2");
+    break;
+  case 3:
+    alert("Zdesi 3");
+    break;
+  case 4:
+    alert("Zdesi 4");
+    break;
+  case 5:
+    alert("Zdesi 5");
+    break;
+  case 6:
+    alert("Zdesi 6");
+    break;
+  case 7:
+    alert("Zdesi 7");
+    break;
+  case 8:
+    alert("Zdesi 8");
+    break;
+  case 9:
+    alert("Zdesi 9");
+    break;
+  default:
+    alert("Sovpadenii ne bilo");
+}
