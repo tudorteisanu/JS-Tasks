1723549857040fdc9f17b71e87797d9a6e4cd105
Denis Postolachi <deny.postolachi88@gmail.com>
2021-08-23 01:53:49 +0300

    seventh task add

---
+++ Задача №7 - условный оператор/script/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const age = 14;
+
+if (!(age >= 14 && age <= 90)) {
+  alert("age вне диапозона");
+} else {
+  alert("age в диапозоне");
+}
+
+/*
+
+if (age >= 14 && age <= 90) {
+  alert("age в диапозоне");
+} else {
+  alert("age вне диапозона");
+}
+ */
